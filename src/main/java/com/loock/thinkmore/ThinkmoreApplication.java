c255abb56e1521bd57088533deaa0bf06919ce3b
wanglu <wanglu@infisa.cn>
2020-10-15 10:50:20 +0800

    Initial commit

---
+++ src/main/java/com/loock/thinkmore/ThinkmoreApplication.java
@@ -0,0 +1,13 @@
+package com.loock.thinkmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkmoreApplication.class, args);
+    }
+
+}
