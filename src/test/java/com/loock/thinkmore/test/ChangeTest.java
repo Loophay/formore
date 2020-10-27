e7b17590616e7cb30fb5614f320c14bb2c64f07a
wanglu <wanglu@infisa.cn>
2020-10-27 15:48:34 +0800

    添加了测试文件

---
+++ src/test/java/com/loock/thinkmore/test/ChangeTest.java
@@ -0,0 +1,11 @@
+package com.loock.thinkmore.test;
+
+import org.junit.jupiter.api.Test;
+
+public class ChangeTest {
+
+    @Test
+    public void Test(){
+        System.out.println("用于测试提交");
+    }
+}
